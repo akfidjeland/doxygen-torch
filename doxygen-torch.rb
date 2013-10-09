a22aa8654d745fcdffbf19585af99686db98e589
Julien Cornebise <julien.cornebise@gmail.com>
2013-10-09 15:08:06 +0100

    Explicitely bump version to 0.1.1

--- doxygen-torch.rb
+++ doxygen-torch.rb
@@ -4,6 +4,7 @@ require 'formula'
 
 class DoxygenTorch < Formula
   url 'https://raw.github.com/akfidjeland/doxygen-torch/master/doxygen-torch-0.1.1.tar.gz'
+  version "0.1.1"
   sha1 '90db15f4ee8bd13ede6698abb6a5bec2d7911440'
   homepage 'https://github.com/akfidjeland/doxygen-torch'
   head 'https://github.com/akfidjeland/doxygen-torch.git'
