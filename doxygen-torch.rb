c802644fb9675cc6c0f33497dcfda5c3ee6bc35e
Andreas Fidjeland <akfidjeland@fastmail.fm>
2013-11-11 17:03:02 +0000

    Updated sha1 for v0.1.4

--- doxygen-torch.rb
+++ doxygen-torch.rb
@@ -4,8 +4,8 @@ require 'formula'
 
 class DoxygenTorch < Formula
   url 'https://github.com/akfidjeland/doxygen-torch/archive/v0.1.3.tar.gz'
-  version "0.1.3"
-  sha1 '8c9bf1a7dbb62f7a01fb14c789f64db265b4a39c'
+  version "0.1.4"
+  sha1 'ff9cb9dcb01ed4467d213b1faf8343e5a06d9690'
   homepage 'https://github.com/akfidjeland/doxygen-torch'
   head 'https://github.com/akfidjeland/doxygen-torch.git'
 
